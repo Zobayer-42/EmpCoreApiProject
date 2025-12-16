f8cfeb1d79a096cba6b0b249ff13d571d71db439
Zobayer-42 <20234203140@cse.bubt.edu.bd>
2025-12-17 02:53:36 +0600

    add to api endpoint

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -70,5 +70,54 @@ namespace EmpCoreApiProject.Controllers
             }
             return Ok(departmentEntry);
         }
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult UpdateDepartment(Guid id, Department department)
+        {
+            var existingDepartment = _dbContext.Departments
+                .Include(d => d.EmployeeDetails)
+                .FirstOrDefault(d => d.DepartmentId == id);
+            if (existingDepartment == null)
+            {
+                return NotFound();
+            }
+            existingDepartment.Name = department.Name;
+            existingDepartment.Description = department.Description;
+            _dbContext.EmployeeDetails.RemoveRange(existingDepartment.EmployeeDetails);
+            if (department.EmployeeDetails != null)
+            {
+                foreach (var empDetail in department.EmployeeDetails)
+                {
+                    var existEmployee = _dbContext.Employees.Find(empDetail.EmployeeId);
+                    if (existEmployee != null)
+                    {
+                        EmployeeDetail employeeDetail = new EmployeeDetail
+                        {
+                            EmployeeId = existEmployee.EmployeeId,
+                            DepartmentId = existingDepartment.DepartmentId
+                        };
+                        _dbContext.EmployeeDetails.Add(employeeDetail);
+                    }
+                }
+                _dbContext.SaveChanges();
+            }
+            return Ok(existingDepartment);
+        }
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteDepartment(Guid id)
+        {
+            var existingDepartment = _dbContext.Departments
+                .Include(d => d.EmployeeDetails)
+                .FirstOrDefault(d => d.DepartmentId == id);
+            if (existingDepartment == null)
+            {
+                return NotFound();
+            }
+            _dbContext.EmployeeDetails.RemoveRange(existingDepartment.EmployeeDetails);
+            _dbContext.Departments.Remove(existingDepartment);
+            _dbContext.SaveChanges();
+            return Ok(existingDepartment);
+        }
     }
 }
